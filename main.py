82b646b1cefe62b86a8230da2107b2c782529b60
Lisukod <45397160+Lisukod@users.noreply.github.com>
2020-11-01 19:31:47 +0100

    redukcja if'a sprawdzajacego puste kilogramy

--- main.py
+++ main.py
@@ -9,6 +9,12 @@ empty_kilos_most = {
     "empty_kilos": 0
 }
 
+def is_box_emptier():
+    if empty_kilos_most["empty_kilos"] < (max_box_capacity - current_box):
+            empty_kilos_most["box"] = box_counter
+            empty_kilos_most["empty_kilos"] = (max_box_capacity - current_box)
+
+
 print("Podaj wage towaru do zapakowania. \nMin. 1kg \nMax. 10kg")
 
 while True:
@@ -17,22 +23,18 @@ while True:
         if current_box > 0:
             box_counter += 1
             sent_kilos += current_box
-            if empty_kilos_most["empty_kilos"] < (max_box_capacity - current_box):
-                empty_kilos_most["box"] = box_counter
-                empty_kilos_most["empty_kilos"] = (max_box_capacity - current_box)  
+            is_box_emptier()
         break
     elif weight_of_element < 0:
         print("Podales ujemną wartosć paczki. Proszę podac faktyczna wartosc")
         continue
     elif weight_of_element < 1 or weight_of_element > 10:
-        print("Waga towaru do zapakowania wychodzi poza zakres")
+        print("Waga towaru do zapakowania wychodzi poza zakres 1-10kg")
         continue
     
     if (current_box + weight_of_element) > max_box_capacity:
         sent_kilos += current_box
-        if empty_kilos_most["empty_kilos"] < (max_box_capacity - current_box):
-            empty_kilos_most["box"] = box_counter
-            empty_kilos_most["empty_kilos"] = (max_box_capacity - current_box)
+        is_box_emptier()
         box_counter += 1
         current_box = weight_of_element
     else:
@@ -42,5 +44,3 @@ print("Wysłane paczki: {}".format(box_counter))
 print("Wysłane kilogramy: {}".format(sent_kilos))
 print("Puste kilogramy: {}".format( box_counter*20-sent_kilos))
 print("W paczce nr. {} jest najwięcej pustych kg {}".format(empty_kilos_most["box"], empty_kilos_most["empty_kilos"]))
-
-
