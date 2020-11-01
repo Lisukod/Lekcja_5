c71b0460fafe383f235012891fcd807e677d4748
Lisukod <45397160+Lisukod@users.noreply.github.com>
2020-11-01 19:24:54 +0100

    Skrócenie kodu, dodanie słownika pustych elementów

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import sys
 
 max_box_capacity = int(sys.argv[1])
 box_counter = 0
-current_box = 0 
-empty_kilos = 0
+current_box = 0
 sent_kilos = 0
-empty_kilos_max = 0
+empty_kilos_most = {
+    "box": 0,
+    "empty_kilos": 0
+}
 
 print("Podaj wage towaru do zapakowania. \nMin. 1kg \nMax. 10kg")
 
@@ -15,9 +17,9 @@ while True:
         if current_box > 0:
             box_counter += 1
             sent_kilos += current_box
-            empty_kilos += (max_box_capacity - current_box)
-            if empty_kilos_max < (max_box_capacity - current_box):
-                empty_kilos_max = (max_box_capacity - current_box)  
+            if empty_kilos_most["empty_kilos"] < (max_box_capacity - current_box):
+                empty_kilos_most["box"] = box_counter
+                empty_kilos_most["empty_kilos"] = (max_box_capacity - current_box)  
         break
     elif weight_of_element < 0:
         print("Podales ujemną wartosć paczki. Proszę podac faktyczna wartosc")
@@ -27,18 +29,18 @@ while True:
         continue
     
     if (current_box + weight_of_element) > max_box_capacity:
-        box_counter += 1
         sent_kilos += current_box
-        empty_kilos += (max_box_capacity - current_box)
-        if empty_kilos_max < (max_box_capacity - current_box):
-            empty_kilos_max = (max_box_capacity - current_box)
+        if empty_kilos_most["empty_kilos"] < (max_box_capacity - current_box):
+            empty_kilos_most["box"] = box_counter
+            empty_kilos_most["empty_kilos"] = (max_box_capacity - current_box)
+        box_counter += 1
         current_box = weight_of_element
     else:
         current_box += weight_of_element
 
 print("Wysłane paczki: {}".format(box_counter))
 print("Wysłane kilogramy: {}".format(sent_kilos))
-print("Puste kilogramy: {}".format(empty_kilos))
-print("Najwięcej wysłanych pustych kilogramów: {}".format(empty_kilos_max))
+print("Puste kilogramy: {}".format( box_counter*20-sent_kilos))
+print("W paczce nr. {} jest najwięcej pustych kg {}".format(empty_kilos_most["box"], empty_kilos_most["empty_kilos"]))
 
 
